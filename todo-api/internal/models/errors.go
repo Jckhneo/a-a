f3d998f9d7f228daece559313521afd37dae3ad3
unknown <usuario1@M26-05-64-10674.utadeo.loc>
2026-04-29 17:45:02 -0500

    actualisasion del bentinuebe de abril del dosmilbentiseis

--- todo-api/internal/models/errors.go
+++ todo-api/internal/models/errors.go
@@ -4,16 +4,16 @@ import "errors"
 
 // Errores del dominio (modelos)
 var (
-	ErrEmptyTitle      = errors.New("el título no puede estar vacío")
-	ErrTitleTooLong    = errors.New("el título no puede superar 150 caracteres")
-	ErrInvalidPriority = errors.New("prioridad inválida: usa 'baja', 'media' o 'alta'")
-	ErrMissingFields   = errors.New("username, email y password son obligatorios")
+	ErrEmptyTitle       = errors.New("el título no puede estar vacío")
+	ErrTitleTooLong     = errors.New("el título no puede superar 150 caracteres")
+	ErrInvalidPriority  = errors.New("prioridad inválida: usa 'baja', 'media' o 'alta'")
+	ErrMissingFields    = errors.New("username, email y password son obligatorios")
 	ErrPasswordTooShort = errors.New("la contraseña debe tener al menos 6 caracteres")
 )
 
-// Errores de repositorio
+// Errores de repositorio añaaaaaaaa
 var (
-	ErrNotFound      = errors.New("recurso no encontrado")
-	ErrDuplicate     = errors.New("el recurso ya existe")
-	ErrUnauthorized  = errors.New("no autorizado")
+	ErrNotFound     = errors.New("recurso no encontrado")
+	ErrDuplicate    = errors.New("el recurso ya existe")
+	ErrUnauthorized = errors.New("no autorizado")
 )
